713c11f1ebd061109aa63004fcde3d5431b0a3b4
dreamycactus <hello_great_man@yahoo.ca>
2014-04-26 10:45:17 -0400

    First commit

---
+++ Assets/Scripts/BarnMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarnMove : MonoBehaviour {
+	public float maxspeed = 10.0f;
+	public float accel = 0.2f;
+
+	Rigidbody2D rbody;
+	// Use this for initialization
+	void Start () {
+		rbody = rigidbody2D;
+		rbody.fixedAngle = true;
+	}
+
+	public void Move(int dir) {
+		rbody.AddForce (new Vector2 ( (float)dir * 0.2f, 0.0f) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+	}
+}
---
+++ Assets/Scripts/BearController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearController : MonoBehaviour {
+
+	private Animator animator;
+	private BarnMove barnMove;
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+		barnMove = GetComponent<BarnMove> ();
+	}
+
+	void HandleInput()
+	{
+		var idx = Input.GetAxis ("Horizontal");
+		var idy = Input.GetAxis("Vertical");
+
+		if (transform.localScale.x > 0) {
+			FlipSprite();
+		}
+		
+		barnMove.Move(AxisRound(idx) );
+			
+//		else
+//		{
+//			
+//		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		HandleInput();
+
+	}
+
+	int AxisRound(float val) 
+	{
+		Debug.Log (Mathf.Sign (val));
+		if (Mathf.Abs(val) < 0.3) {
+			return 0; 
+		}
+
+		return Mathf.Sign (val);
+	}
+	private void FlipSprite(){
+		transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
+	}
+}
