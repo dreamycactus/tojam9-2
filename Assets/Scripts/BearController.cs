dfea3629f7ad208fa32773461f535418e3aa9059
dreamycactus <hello_great_man@yahoo.ca>
2014-04-26 11:10:10 -0400

    Controller fix

--- Assets/Scripts/BearController.cs
+++ Assets/Scripts/BearController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class BearController : MonoBehaviour {
-
+	
 	private Animator animator;
 	private BarnMove barnMove;
 	// Use this for initialization
@@ -11,39 +11,37 @@ public class BearController : MonoBehaviour {
 		animator = this.GetComponent<Animator>();
 		barnMove = GetComponent<BarnMove> ();
 	}
-
+	
 	void HandleInput()
 	{
 		var idx = Input.GetAxis ("Horizontal");
 		var idy = Input.GetAxis("Vertical");
-
+		
 		if (transform.localScale.x > 0) {
 			FlipSprite();
 		}
 		
 		barnMove.Move(AxisRound(idx) );
-			
-//		else
-//		{
-//			
-//		}
+		
+		//		else
+		//		{
+		//			
+		//		}
 	}
-
+	
 	// Update is called once per frame
 	void Update()
 	{
 		HandleInput();
-
+		
 	}
-
+	
 	int AxisRound(float val) 
 	{
-		Debug.Log (Mathf.Sign (val));
 		if (Mathf.Abs(val) < 0.3) {
 			return 0; 
 		}
-
-		return Mathf.Sign (val);
+		return (int)Mathf.Sign(val);
 	}
 	private void FlipSprite(){
 		transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
